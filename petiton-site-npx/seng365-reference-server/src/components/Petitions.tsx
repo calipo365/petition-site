0180e33365b3fc364ac60485d4f65118157965b4
calipo365 <nina.c.day@gmail.com>
2024-05-10 21:35:19 +1200

    Sorting working!

--- petiton-site-npx/seng365-reference-server/src/components/Petitions.tsx
+++ petiton-site-npx/seng365-reference-server/src/components/Petitions.tsx
@@ -41,17 +41,30 @@ const costGroups = [
     { label: "Over $50", min: 51, max: Infinity }
 ];
 
+const SortGroups = [
+    { label: "Petition name A-Z"}, 
+    { label: "Petition name Z-A"},
+    { label: "Price - Lowest first"},
+    { label: "Price - Highest first"},
+    { label: "Oldest posted" },
+    { label: "Newest posted" }
+]
+
 const Petitions = () => {
     const navigate = useNavigate();
     const theme = useTheme();
 
     const [selectedCategories, setSelectedCategories] = React.useState<number[]>([]);
     const [categories, setCategories] = React.useState<Category[]>([]);
+    const [sort, setSort] = React.useState("Petition name A-Z")
     const [selectedCostGroups, setSelectedCostGroups] = React.useState<string[]>([]);
     const [petitions, setPetitions] = React.useState<Petition[]>([]);
     const [errorFlag, setErrorFlag] = React.useState(false)
     const [errorMessage, setErrorMessage] = React.useState(" ")
     const [title, setTitle]= React.useState("")
+
+    const [currentPage, setCurrentPage] = React.useState(1);
+    const petitionsPerPage = 9;
 
     React.useEffect(() => {
         getPetitions()
@@ -102,7 +115,30 @@ const Petitions = () => {
             return selectedCostGroups.length === 0 || filterByCostGroup(lowestCost);
         });
 
-        return filteredPetitions.map((item: Petition) => (
+        const sortedPetitions = filteredPetitions.sort((a, b) => {
+            switch (sort) {
+                case "Petition name A-Z":
+                    return a.title.localeCompare(b.title)
+                case "Petition name Z-A":
+                    return b.title.localeCompare(a.title);
+                case "Price - Lowest first":
+                    return a.supportingCost - b.supportingCost;
+                case "Price - Highest first":
+                    return b.supportingCost - a.supportingCost;
+                case "Oldest posted":
+                    return new Date(a.creationDate).getTime() - new Date(b.creationDate).getTime();
+                case "Newest posted":
+                    return new Date(b.creationDate).getTime() - new Date(a.creationDate).getTime();
+                default:
+                    return 0;
+            }
+        });
+
+        const startIndex = (currentPage - 1) * petitionsPerPage;
+        const endIndex = startIndex + petitionsPerPage;
+        const currentPetitions = sortedPetitions.slice(startIndex, endIndex);
+
+        return currentPetitions.map((item: Petition) => (
             <div key={item.petitionId} className="petition">
                 <img 
                     src={`http://localhost:4941/api/v1/petitions/${item.petitionId}/image`}
@@ -156,6 +192,13 @@ const Petitions = () => {
         setSelectedCostGroups(typeof value === 'string' ? value.split(',') : value);
     };
 
+    const handleSortChange = (event: SelectChangeEvent<string>) => {
+        const {
+            target: { value },
+        } = event;
+        setSort(value);
+    };
+
     const getCategoryNameById = (categoryId: number) => {
         const category = categories.find(cat => cat.categoryId === categoryId);
         return category ? category.name : 'Unknown';
@@ -165,101 +208,143 @@ const Petitions = () => {
         setTitle(event.target.value);
     };
 
-    return (
-        <div>
-            <h2>Petition Pledge</h2>
-            <div className="make-petition-container">
-                <button type="button" className="make-button" data-toggle="modal" data-target="#makePetitionModal">
-                        Make your own petition!
-                    </button>
-                        <div className='modal fade' id='makePetitionModal' tabIndex={-1} role="dialog"
-                            aria-labelledby="makePetitionModalLabel" aria-hiddden="true">
-                                <div className="modal-dialog" role="document">
-                                    <div className="modal-content">
-                                        <div className="modal-header">
-                                            <h5 className='modal-title' id='makePetitionModalLabel'>Make a petition</h5>
-                                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
+    const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
+        setCurrentPage(value);
+    }
+
+    if (errorFlag) {
+        return (
+            <div>
+                <h2> Petiton Pledge </h2>
+                <div style={{ color: "red" }}>
+                    {errorMessage}
+                </div>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <h2>Petition Pledge</h2>
+                <div className="make-petition-container">
+                    <button type="button" className="make-button" data-toggle="modal" data-target="#makePetitionModal">
+                            Make your own petition!
+                        </button>
+                            <div className='modal fade' id='makePetitionModal' tabIndex={-1} role="dialog"
+                                aria-labelledby="makePetitionModalLabel" aria-hiddden="true">
+                                    <div className="modal-dialog" role="document">
+                                        <div className="modal-content">
+                                            <div className="modal-header">
+                                                <h5 className='modal-title' id='makePetitionModalLabel'>Make a petition</h5>
+                                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div className='modal-footer'>
+                                                <form onSubmit={addPetition}>
+                                                    <input type='text' value={title} onChange={updateTitleState} />
+                                                    <input type='submit' value='Submit' />
+                                                </form>
+                                                <button type='button' className='btn btn-secondary' data-dismiss='modal'>
+                                                    Close
+                                                </button>
+                                            </div>  
                                         </div>
-                                        <div className='modal-footer'>
-                                            <form onSubmit={addPetition}>
-                                                <input type='text' value={title} onChange={updateTitleState} />
-                                                <input type='submit' value='Submit' />
-                                            </form>
-                                            <button type='button' className='btn btn-secondary' data-dismiss='modal'>
-                                                Close
-                                            </button>
-                                        </div>  
-                                    </div>
-                                </div>    
+                                    </div>    
+                                </div>
                             </div>
-                        </div>
-                <div className="filter-container">
-                    <FormControl sx={{ m: 1, width: 300}}>
-                        <InputLabel id="category-multiple-chip-label">Categories</InputLabel>
+                    <div className="filter-container">
+                        <FormControl sx={{ m: 1, width: 300}}>
+                            <InputLabel id="category-multiple-chip-label">Categories</InputLabel>
+                            <Select
+                                labelId='category-multiple-chip-label'
+                                id='category-multiple-chip'
+                                multiple
+                                value={selectedCategories.map(String)}
+                                onChange={handleCategoryChange}
+                                input={<OutlinedInput id='select-multiple-chip' label='Categories' />}
+                                renderValue={(selected) => (
+                                    <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5}}>
+                                        {selected.map((value) => (
+                                            <Chip key={value} label={categories.find(c => c.categoryId.toString() === value)?.name} />
+                                        ))}
+                                    </Box>
+                                )}
+                                MenuProps={MenuProps}
+                            >
+                                {categories.map((category: Category) => (
+                                    <MenuItem
+                                        key={category.categoryId}
+                                        value={category.categoryId.toString()}
+                                        style={getStyles(category.name, selectedCategories.map(String), theme)}
+                                    >
+                                        {category.name}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                        <FormControl sx={{ m: 1, width: 300 }}>
+                        <InputLabel id="cost-group-multiple-chip-label">Cost</InputLabel>
                         <Select
-                            labelId='category-multiple-chip-label'
-                            id='category-multiple-chip'
+                            labelId='cost-group-multiple-chip-label'
+                            id='cost-group-multiple-chip'
                             multiple
-                            value={selectedCategories.map(String)}
-                            onChange={handleCategoryChange}
-                            input={<OutlinedInput id='select-multiple-chip' label='Categories' />}
+                            value={selectedCostGroups}
+                            onChange={handleCostGroupChange}
+                            input={<OutlinedInput id='select-multiple-chip' label='Cost' />}
                             renderValue={(selected) => (
-                                <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5}}>
+                                <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
                                     {selected.map((value) => (
-                                        <Chip key={value} label={categories.find(c => c.categoryId.toString() === value)?.name} />
+                                        <Chip key={value} label={value} />
                                     ))}
                                 </Box>
                             )}
                             MenuProps={MenuProps}
                         >
-                            {categories.map((category: Category) => (
+                            {costGroups.map((group) => (
                                 <MenuItem
-                                    key={category.categoryId}
-                                    value={category.categoryId.toString()}
-                                    style={getStyles(category.name, selectedCategories.map(String), theme)}
+                                    key={group.label}
+                                    value={group.label}
+                                    style={getStyles(group.label, selectedCostGroups, theme)}
                                 >
-                                    {category.name}
+                                    {group.label}
                                 </MenuItem>
                             ))}
                         </Select>
                     </FormControl>
-                    <FormControl sx={{ m: 1, width: 300 }}>
-                    <InputLabel id="cost-group-multiple-chip-label">Cost</InputLabel>
-                    <Select
-                        labelId='cost-group-multiple-chip-label'
-                        id='cost-group-multiple-chip'
-                        multiple
-                        value={selectedCostGroups}
-                        onChange={handleCostGroupChange}
-                        input={<OutlinedInput id='select-multiple-chip' label='Cost' />}
-                        renderValue={(selected) => (
-                            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-                                {selected.map((value) => (
-                                    <Chip key={value} label={value} />
-                                ))}
-                            </Box>
-                        )}
-                        MenuProps={MenuProps}
-                    >
-                        {costGroups.map((group) => (
-                            <MenuItem
-                                key={group.label}
-                                value={group.label}
-                                style={getStyles(group.label, selectedCostGroups, theme)}
+                    </div>
+                    <div className='sort-container'>
+                        <FormControl sx={{ m: 1, width: 300 }}>
+                            <InputLabel id="sort-select-label">Sort By</InputLabel>
+                            <Select
+                                labelId='sort-select-label'
+                                id='sort-select'
+                                value={sort}
+                                onChange={handleSortChange}
+                                label='Sort By'
                             >
-                                {group.label}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                </FormControl>
-                </div>
-                <div className="petition-container">
-                    <div className="petition-grid">{list_of_petitions()}</div>
-                </div>
-        </div>
-    )
+                                {SortGroups.map((group) => (
+                                    <MenuItem key={group.label} value={group.label}>
+                                        {group.label}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </div>
+                    <div className="petition-container">
+                        <div className="petition-grid">{list_of_petitions()}</div>
+                        <Stack spacing={2} alignItems="center">
+                        <Pagination
+                            count={Math.ceil(petitions.length / petitionsPerPage)}
+                            page={currentPage}
+                            onChange={handlePageChange}
+                            variant="outlined"
+                            shape="rounded"
+                        />
+                    </Stack>
+                    </div>
+            </div>
+        )
+    }
 }
 
 export default Petitions;
